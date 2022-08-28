695c75e1ea0a38f31b26bcb7521e8387d0f13720
Kristian Feldsam <feldsam@gmail.com>
2022-08-28 15:48:28 +0200

    vmm/kvm/restore - added check for multipath device target
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- scripts/install-5.6.0.sh
+++ scripts/install-5.6.0.sh
@@ -38,10 +38,13 @@ chown oneadmin: "${ONE_VAR}/remotes/vmm/kvm/"snapshot_*-3par
 echo "*** Copy VMM deploy and attach_disk scripts to ${ONE_VAR}/remotes/vmm/kvm/ ..."
 cp $CP_ARG "$CWD/vmm/kvm/deploy" "${ONE_VAR}/remotes/vmm/kvm/"
 cp $CP_ARG "$CWD/vmm/kvm/attach_disk" "${ONE_VAR}/remotes/vmm/kvm/"
+cp $CP_ARG "$CWD/vmm/kvm/restore" "${ONE_VAR}/remotes/vmm/kvm/"
 chmod a+x "${ONE_VAR}/remotes/vmm/kvm/deploy"
 chmod a+x "${ONE_VAR}/remotes/vmm/kvm/attach_disk"
+chmod a+x "${ONE_VAR}/remotes/vmm/kvm/restore"
 chown oneadmin: "${ONE_VAR}/remotes/vmm/kvm/deploy"
 chown oneadmin: "${ONE_VAR}/remotes/vmm/kvm/attach_disk"
+chown oneadmin: "${ONE_VAR}/remotes/vmm/kvm/restore"
 
 echo "*** Copy checkMultipath.py and dmmp.py scripts to ${ONE_VAR}/remotes/vmm/ ..."
 cp $CP_ARG "$CWD/vmm/checkMultipath.py" "${ONE_VAR}/remotes/vmm/"
